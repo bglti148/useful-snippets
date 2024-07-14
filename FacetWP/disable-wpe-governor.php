c8a2efc8e58707c8617ebefc9fd6491bbf164a15
bglti148 <bglti148@gmail.com>
2024-07-14 17:05:37 +0000

    FacetWP snippets

---
+++ FacetWP/disable-wpe-governor.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Add this code to the wp-config.php (not to functions.php) file of the WordPress install
+ * to disable the WP Engien Governor
+ */
+
+define( 'WPE_GOVERNOR', false );
